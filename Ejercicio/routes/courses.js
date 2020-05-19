cd85e14bad61a58accdd2603400e9568d6e9b0e7
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-05-19 13:29:54 -0500

    Curaca first change

--- Ejercicio/routes/courses.js
+++ Ejercicio/routes/courses.js
@@ -3,15 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('courses', { title: 'Courses' });
+  res.render('courses', function(err, html) {
+    res.render('templates/layout', {
+      'seccion': html
+    })
+  });
 });
 
 router.get('/level', function(req, res, next) {
-    res.render('courses-level', { title: 'Level' });
+  res.render('courses-level', function(err, html) {
+    res.render('templates/layout', {
+      'seccion': html
+    })
   });
+});
 
-  router.get('/pay', function(req, res, next) {
-    res.render('courses-pay', { title: 'Pay' });
+router.get('/pay', function(req, res, next) {
+  res.render('courses-pay', function(err, html) {
+    res.render('templates/layout', {
+      'seccion': html
+    })
   });
+});
 
 module.exports = router;
