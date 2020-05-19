620bd22207263794412b904e01a2ea4d6d727922
EdwinWilliamArevalo <edwin.arevalo@tecsup.edu.pe>
2020-05-19 17:35:28 -0500

    finish commit -Arevalo

--- Ejercicio/routes/index.js
+++ Ejercicio/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', function(err, html) {
+    res.render('templates/layout', {
+      'seccion': html
+    })
+  });
 });
-
 module.exports = router;
