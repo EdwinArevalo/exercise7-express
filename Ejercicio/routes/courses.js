b3af7e8ecf6d5d1d8a2b346d390a4ca3b6f0449d
Friman Vilcapoma Flores <friman.vilcapoma@tecsup.edu.pe>
2020-06-02 07:36:40 -0500

    Commit

--- Ejercicio/routes/courses.js
+++ Ejercicio/routes/courses.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+let inscription = {
+  course: '',
+  modules: [],
+  paymentType: 0,
+  amount: 0
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('courses', function(err, html) {
@@ -8,6 +15,24 @@ router.get('/', function(req, res, next) {
       'seccion': html
     })
   });
+}).post('/',(req, res)=>{
+  
+   if (req.body.course == 1){
+    inscription.course = 'Java';
+    inscription.amount = 1200;
+   }
+   if (req.body.course == 2){
+    inscription.course = 'Php';
+    inscription.amount = 800;
+  }
+  if (req.body.course == 3){
+    inscription.course = '.NET';
+    inscription.amount = 1500;
+
+  }
+  console.log(inscription);
+  res.redirect('/courses/level');
+
 });
 
 router.get('/level', function(req, res, next) {
@@ -16,6 +41,28 @@ router.get('/level', function(req, res, next) {
       'seccion': html
     })
   });
+}).post('/level',(req, res)=>{
+   if(req.body.basic){
+    inscription.modules.push(req.body.basic);
+   }
+   if(req.body.intermediate){
+    inscription.modules.push(req.body.intermediate);
+   }
+   if(req.body.avanced){
+     inscription.modules.push(req.body.avanced);
+   }
+   if(inscription.course == 'Php'){
+     inscription.amount = (inscription.modules.length) *  inscription.amount;
+   }
+   if(inscription.course == 'Java'){
+    inscription.amount = (inscription.modules.length) *  inscription.amount;
+  }
+  if(inscription.course == '.NET'){
+    inscription.amount = (inscription.modules.length) *  inscription.amount;
+  }
+   console.log(inscription);
+   res.redirect('/courses/pay');
+
 });
 
 router.get('/pay', function(req, res, next) {
@@ -24,6 +71,15 @@ router.get('/pay', function(req, res, next) {
       'seccion': html
     })
   });
+}).post('/pay',(req, res)=>{
+
+  req.body.type == 'Effective' ? inscription.paymentType = req.body.type: inscription.paymentType = req.body.type;
+  console.log(inscription);
+  if(inscription.paymentType == 'Effective'){
+    inscription.amount = inscription.amount - (inscription.amount *0.1);
+  }
+  console.log(inscription);
+  res.render('result',{inscription});
 });
 
 module.exports = router;
